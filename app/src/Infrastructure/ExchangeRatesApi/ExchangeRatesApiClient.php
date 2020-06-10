48553076f07a5162b2c074e60993db2a0649d364
Alexandre Dupuy <alex.dpy@gmail.com>
2020-06-10 17:00:44 +0200

    exchangesrateapi.io http client

--- app/src/Infrastructure/ExchangeRatesApi/ExchangeRatesApiClient.php
+++ app/src/Infrastructure/ExchangeRatesApi/ExchangeRatesApiClient.php
@@ -2,14 +2,50 @@
 
 namespace App\Infrastructure\ExchangeRatesApi;
 
+use App\Domain\Exception\ExchangeRateNotFoundException;
 use App\Domain\Model\Currency;
 use App\Domain\Model\ExchangeRate;
 use App\Domain\Repository\ExchangeRateReader;
+use Symfony\Contracts\HttpClient\Exception\HttpExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class ExchangeRatesApiClient implements ExchangeRateReader
 {
+    /**
+     * @var HttpClientInterface
+     */
+    private $httpClient;
+
+    public function __construct(HttpClientInterface $httpClient)
+    {
+        $this->httpClient = $httpClient;
+    }
+
     public function get(Currency $baseCurrency, Currency $currency, \DateTimeInterface $date): ExchangeRate
     {
-        return new ExchangeRate();
+        $url = sprintf(
+            'https://api.exchangeratesapi.io/%s?base=%s',
+            $date->format('Y-m-d'),
+            $baseCurrency->getSymbol()
+        );
+
+        $response = $this->httpClient->request('GET', $url);
+
+        try {
+            $content = json_decode($response->getContent(), true);
+
+            // TODO compare dates resquest<->response and throw ExchangeRateNotFoundException
+
+            // TODO check exists $content[$currency->getSymbol()]
+
+            return new ExchangeRate(
+                $baseCurrency,
+                $currency,
+                $date,
+                $content['rates'][$currency->getSymbol()]
+            );
+        } catch (HttpExceptionInterface $e) {
+            throw new ExchangeRateNotFoundException($e->getMessage(), $e->getCode(), $e);
+        }
     }
 }
---
+++ app/tests/Infrastructure/ExchangeRatesApi/ExchangeRatesApiClientTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Tests\Infrastructure\ExchangeRatesApi;
+
+use App\Domain\Exception\ExchangeRateNotFoundException;
+use App\Domain\Model\Currency;
+use App\Domain\Model\ExchangeRate;
+use App\Infrastructure\ExchangeRatesApi\ExchangeRatesApiClient;
+use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
+use Symfony\Contracts\HttpClient\Exception\HttpExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+
+class ExchangeRatesApiClientTest extends TestCase
+{
+    /**
+     * @var ExchangeRatesApiClient
+     */
+    private $SUT;
+
+    private $httpClient;
+
+    protected function setUp()
+    {
+        $this->httpClient = $this->prophesize(HttpClientInterface::class);
+
+        $this->SUT = new ExchangeRatesApiClient($this->httpClient->reveal());
+    }
+
+    /**
+     * @test
+     */
+    public function itGetsTheRateFromTheApi()
+    {
+        // Arrange
+        $baseCurrency = new Currency('EUR');
+        $currency = new Currency('USD');
+        $date = new \DateTimeImmutable('2020-06-02');
+
+        $url = 'https://api.exchangeratesapi.io/2020-06-02?base=EUR';
+
+        $response = $this->prophesize(ResponseInterface::class);
+        $response->getContent()->willReturn(json_encode([
+            'rates' => ['USD' => 1.1174],
+            'base' => 'EUR',
+            'date' => '2020-06-02',
+        ]));
+
+        $this->httpClient->request('GET', $url, Argument::any())->willReturn($response->reveal());
+        $expectedResult = new ExchangeRate($baseCurrency, $currency, $date, 1.1174);
+
+        // Act
+        $result = $this->SUT->get($baseCurrency, $currency, $date);
+
+        // Assert
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function itThrowsANotFoundExceptionIfTheApiCallHasFailed()
+    {
+        // Arrange
+        $baseCurrency = new Currency('EUR');
+        $currency = new Currency('USD');
+        $date = new \DateTimeImmutable('2020-06-02');
+
+        $url = 'https://api.exchangeratesapi.io/2020-06-02?base=EUR';
+
+        $response = $this->prophesize(ResponseInterface::class);
+        $response->getContent()->willThrow($this->prophesize(HttpExceptionInterface::class)->reveal());
+
+        $this->httpClient->request('GET', $url, Argument::any())->willReturn($response->reveal());
+
+        // Assert
+        $this->expectException(ExchangeRateNotFoundException::class);
+
+        // Act
+        $this->SUT->get($baseCurrency, $currency, $date);
+    }
+}
+
