254e3965c20cc3bfa31d92b7b3750515223de834
Alexandre Dupuy <alex.dpy@gmail.com>
2020-06-11 23:32:53 +0200

    protect application with Basic Auth

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Swarrot\SwarrotBundle\SwarrotBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
